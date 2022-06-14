0ec506793a29cefa1ab16a1dd280e491f9faf0ca
GameGodS3 <sudevssuresh@gmail.com>
2022-06-14 21:17:51 +0530

    Added: mudrika.herokuapp.com to ALLOWED_HOSTS

--- Mudrika/settings.py
+++ Mudrika/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i0l0r^1c4^)234w^_fgjz=!pfwqamux!!omeutx4x@n4c7f)cu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'mudrika.herokuapp.com']
 
 
 # Application definition
