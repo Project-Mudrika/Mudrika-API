824a407c2807e888e8fabaa62313f756a6784fc0
Deepudeepthi555 <deepikamohandeepz99@gmail.com>
2022-06-18 09:47:03 +0530

    Added: Rest_API_Frame work

--- MudrikaApi/admin.py
+++ MudrikaApi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Sudu, subscribers
 
 # Register your models here.
+admin.site.register(Sudu)
+admin.site.register(subscribers)
+
--- MudrikaApi/models.py
+++ MudrikaApi/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+
+class Sudu(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
+    slug = models.SlugField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        to_assign = slugify(self.title)
+
+        if Sudu.objects.filter(slug=to_assign).exists():
+            to_assign = to_assign+str(Sudu.objects.all().count())
+
+        self.slug = to_assign
+        super().save(**args, **kwargs)
+
+
+class subscribers(models.Model):
+    sudu = models.ForeignKey(to=Sudu, on_delete=models.DO_NOTHING)
+    email = models.EmailField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+        def __str__(self):
+            return self.email
