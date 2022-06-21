097afff073f90c962ed91a2e67ad55a0325e3a9d
GameGodS3 <sudevssuresh@gmail.com>
2022-06-21 10:08:24 +0530

    Added: User Profile Dummy model

--- MudrikaApi/admin.py
+++ MudrikaApi/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Sudu, subscribers
+from .models import *
 
 # Register your models here.
 admin.site.register(Sudu)
 admin.site.register(subscribers)
+admin.site.register(UserProfileDummy)
 
+
+class UserProfileDummyAdmin(admin.ModelAdmin):
+    list_display = ("acc_address", "first_name", "last_name")
--- MudrikaApi/models.py
+++ MudrikaApi/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 # Create your models here.
+# 0xd1CfE5c03730C4F93ag76a7d8424bA9122Bb4742
+
+
+class UserProfileDummy(models.Model):
+    acc_address = models.CharField(max_length=50, primary_key=True)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    state = models.CharField(max_length=60)
+    district = models.CharField(max_length=60)
+    username = models.CharField(max_length=24)
+    type_phrase = models.CharField(max_length=16)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
 
 
 class Sudu(models.Model):
