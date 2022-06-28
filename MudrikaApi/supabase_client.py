692a33ba3053312bd208c13355802d23aff95d70
aryasreess <aryasreess22@gmail.com>
2022-06-28 12:32:50 +0530

    added:single user data fetch

--- MudrikaApi/supabase_client.py
+++ MudrikaApi/supabase_client.py
@@ -4,6 +4,13 @@ url: str = os.environ.get("https://netlauydpmmztazzzldy.supabase.co")
 key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldGxhdXlkcG1tenRhenp6bGR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTUyODE4MTgsImV4cCI6MTk3MDg1NzgxOH0.EOi8V57Djyl7qSUwPHtrjvUn18keF2gVMeRkwnOB1cQ")
 supabase: Client = create_client("https://netlauydpmmztazzzldy.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldGxhdXlkcG1tenRhenp6bGR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTUyODE4MTgsImV4cCI6MTk3MDg1NzgxOH0.EOi8V57Djyl7qSUwPHtrjvUn18keF2gVMeRkwnOB1cQ")
 
+def fetch_single_user_data(accid):
+    data1 = supabase.table('authority').select('accid').eq('accid',accid).execute()
+    #assert len(data.get("data", [])) > 0
+    print(data1)
+    return data1
+
+
 def insert_into_db(acid,level,fnam,lnam,sta,dis,user):
     data = supabase.table('authority').insert({'accid': acid,'level' : level, 'fname': fnam ,'lname' :lnam ,'state' : sta , 'district' : dis , 'username' : user}).execute()
     return data
@@ -12,9 +19,12 @@ def fetch_data():
     data = supabase.table('authority').select('accid','level','fname','lname','state','district','username').execute()
     print(data)
     print('\n')
+    return data
 
-insert_into_db("1009","national","Devu","diya","kerala","tvm","devudiya")
-fetch_data()
+ 
+#insert_into_db("1010","national","Devu","diya","kerala","tvm","devudiya")
+#fetch_data()
+# fetch_single_user_data(1009)
 
 
 
