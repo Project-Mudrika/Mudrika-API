7c85c02fe23752dbc8f7972e7bdecef1b47f8141
aryasreess <aryasreess22@gmail.com>
2022-06-28 13:43:19 +0530

    changed: fetch all details of single user

--- MudrikaApi/supabase_client.py
+++ MudrikaApi/supabase_client.py
@@ -1,5 +1,7 @@
 import os
 from supabase import create_client, Client
+
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -23,7 +25,7 @@ def fetch_all_data():
     return data
 
 def fetch_single_user_data(accid):
-    data1 = supabase.table('authority').select('accid').eq('accid',accid).execute()
+    data1 = supabase.table('authority').select('accid,level,fname,lname,state,district, username').eq('accid',accid).execute()
     #assert len(data.get("data", [])) > 0
     print(data1)
     return data1
@@ -32,17 +34,17 @@ def fetch_single_user_data(accid):
 
 
 
-# insert_into_db("1009", "national", "Devu", "diya", "kerala", "tvm", "devudiya")
-# fetch_all_data()
+    # insert_into_db("1009", "national", "Devu", "diya", "kerala", "tvm", "devudiya")
+    # fetch_all_data()
 
-#data = supabase.table('authority').select('accid').execute()
-# print(data)
-#data = supabase.table('authority').insert({'accid': '1335', 'fname': "aarya" }).execute()
-# assert if insert response is a success
-#assert data.get("status_code") in (200, 201)
-#data = supabase.table('authority').select('accid').execute()
-# print(data)
+    #data = supabase.table('authority').select('accid').execute()
+    # print(data)
+    #data = supabase.table('authority').insert({'accid': '1335', 'fname': "aarya" }).execute()
+    # assert if insert response is a success
+    #assert data.get("status_code") in (200, 201)
+    #data = supabase.table('authority').select('accid').execute()
+    # print(data)
 
-# bulk insert
-# data = supabase.table('cities').insert([
-#{'name': 'Gotham', 'country_id': 556 },
+    # bulk insert
+    # data = supabase.table('cities').insert([
+    #{'name': 'Gotham', 'country_id': 556 },
