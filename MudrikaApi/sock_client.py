50b45097fc55c4a1272c006d7cabf0bf8f77a3a4
aryasreess <aryasreess22@gmail.com>
2022-11-17 23:34:50 +0530

    added: websocket client code

---
+++ MudrikaApi/sock_client.py
@@ -0,0 +1,14 @@
+import asyncio
+import websockets
+
+async def hello():
+    async with websockets.connect("ws://localhost:8765") as websocket:
+       
+        data = input('Enter latitude,longitude & driverid separated by ":" ') 
+         
+
+
+        await websocket.send(data)
+        await websocket.recv()
+
+asyncio.run(hello())
