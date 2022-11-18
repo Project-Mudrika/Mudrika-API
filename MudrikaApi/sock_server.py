28cd98537de61e12f1e41fb450d976a398a551b2
Ajay Krishna K V <55079486+AJAYK-01@users.noreply.github.com>
2022-11-18 10:07:42 +0530

    store location data of driver in global dict

--- MudrikaApi/sock_server.py
+++ MudrikaApi/sock_server.py
@@ -1,16 +1,23 @@
 import asyncio
 import websockets
 
+location_data = {}
+
+
 async def echo(websocket):
+    global location_data
     async for message in websocket:
         print(message)
-        class_list = dict() 
-        temp = message.split(':') 
+        class_list = dict()
+        temp = message.split(':')
         class_list['latitude'] = temp[0]
         class_list['longitude'] = temp[1]
         class_list['driver_id'] = temp[2]
         print(class_list)
+        location_data.update({temp[2]: [temp[0], temp[1]]})
         await websocket.send(message)
+        print(location_data.get(temp[2]))
+
 
 async def main():
     async with websockets.serve(echo, "localhost", 8765):
